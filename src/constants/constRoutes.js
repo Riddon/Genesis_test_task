6c0576acbc90d92bae282c96d1e9eb0a6305c333
Dmytro Murashko <dmytro.murashko@gmail.com>
2020-10-09 00:20:41 +0300

    Tried add correct deploy

--- src/constants/constRoutes.js
+++ src/constants/constRoutes.js
@@ -1,5 +1,5 @@
 export const routes = {
-    home: {name: 'main', href: '/'},
-    game: {name: 'Game', href: '/game'},
-    result: {name: 'score', href: '/result'}
+    home: {name: 'main', href: '/Genesis_test_task'},
+    game: {name: 'Game', href: '/Genesis_test_task/game'},
+    result: {name: 'score', href: '/Genesis_test_task/result'}
 };
