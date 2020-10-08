196429e19b5d6adee846294534f4c224d4c79d26
Dmytro Murashko <dmytro.murashko@gmail.com>
2020-10-08 23:16:43 +0300

    Updates

---
+++ precache-manifest.3729841fe26f5b5464ab2cf638bc6dd9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "751586dbe6c55dd1eb0e9927a96a2a54",
+    "url": "/Genesis_test_task.git/index.html"
+  },
+  {
+    "revision": "2eed523e3bf74b7ede89",
+    "url": "/Genesis_test_task.git/static/css/main.d8687f18.chunk.css"
+  },
+  {
+    "revision": "cfd38899b820698c3f10",
+    "url": "/Genesis_test_task.git/static/js/2.b4fa1e2e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Genesis_test_task.git/static/js/2.b4fa1e2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eed523e3bf74b7ede89",
+    "url": "/Genesis_test_task.git/static/js/main.77fee483.chunk.js"
+  },
+  {
+    "revision": "db746b1559716227aa34",
+    "url": "/Genesis_test_task.git/static/js/runtime-main.ec370c30.js"
+  },
+  {
+    "revision": "2be9a6eef6af21c40977a9395390e7f7",
+    "url": "/Genesis_test_task.git/static/media/home_page_hand.2be9a6ee.png"
+  }
+]);
